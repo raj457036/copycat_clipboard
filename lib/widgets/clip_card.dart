3e8e0710a851ef7964676d0af0d6112681d4916b
Raj <rs457036@gmail.com>
2024-04-03 12:39:34 +0530

    _

--- lib/widgets/clip_card.dart
+++ lib/widgets/clip_card.dart
@@ -8,67 +8,71 @@ class ClipCard extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Card.outlined(
-      child: InkWell(
-        borderRadius: BorderRadius.circular(12),
-        onTap: () {},
-        child: Column(
-          children: [
-            const Expanded(
-              child: Padding(
-                padding: EdgeInsets.all(padding8),
-                child: Text("Hello World"),
+      child: Column(
+        children: [
+          Expanded(
+            child: InkWell(
+              borderRadius: const BorderRadius.vertical(
+                top: Radius.circular(12),
+              ),
+              onTap: () {},
+              child: const SizedBox.expand(
+                child: Padding(
+                  padding: EdgeInsets.all(padding8),
+                  child: Text("Hello World"),
+                ),
               ),
             ),
-            const Divider(height: 0),
-            SizedBox.fromSize(
-              size: const Size.fromHeight(56),
-              child: DecoratedBox(
-                decoration: const BoxDecoration(
-                  color: Colors.white,
-                ),
-                child: Padding(
-                  padding: const EdgeInsets.all(padding8),
-                  child: Row(
-                    children: [
-                      // const Icon(Icons.abc_rounded),
-                      // width8,
-                      Column(
-                        crossAxisAlignment: CrossAxisAlignment.start,
-                        mainAxisSize: MainAxisSize.min,
-                        children: [
-                          Text(
-                            "Link",
-                            style: context.textTheme.titleSmall,
-                          ),
-                          Text(
-                            "4 days ago",
-                            style: context.textTheme.labelSmall,
-                          ),
-                        ],
-                      ),
-                      const Spacer(),
-                      // IconButton(
-                      //   icon: const Icon(Icons.arrow_outward_rounded),
-                      //   tooltip: "Open link",
-                      //   onPressed: () {},
-                      // ),
-                      // IconButton(
-                      //   icon: const Icon(Icons.download_rounded),
-                      //   tooltip: "Download",
-                      //   onPressed: () {},
-                      // ),
-                      IconButton(
-                        icon: const Icon(Icons.copy),
-                        tooltip: "Copy",
-                        onPressed: () {},
-                      ),
-                    ],
-                  ),
+          ),
+          const Divider(height: 0),
+          SizedBox.fromSize(
+            size: const Size.fromHeight(56),
+            child: DecoratedBox(
+              decoration: const BoxDecoration(
+                color: Colors.white,
+              ),
+              child: Padding(
+                padding: const EdgeInsets.all(padding8),
+                child: Row(
+                  children: [
+                    // const Icon(Icons.abc_rounded),
+                    // width8,
+                    Column(
+                      crossAxisAlignment: CrossAxisAlignment.start,
+                      mainAxisSize: MainAxisSize.min,
+                      children: [
+                        Text(
+                          "Link",
+                          style: context.textTheme.titleSmall,
+                        ),
+                        Text(
+                          "4 days ago",
+                          style: context.textTheme.labelSmall,
+                        ),
+                      ],
+                    ),
+                    const Spacer(),
+                    // IconButton(
+                    //   icon: const Icon(Icons.arrow_outward_rounded),
+                    //   tooltip: "Open link",
+                    //   onPressed: () {},
+                    // ),
+                    // IconButton(
+                    //   icon: const Icon(Icons.download_rounded),
+                    //   tooltip: "Download",
+                    //   onPressed: () {},
+                    // ),
+                    IconButton(
+                      icon: const Icon(Icons.copy),
+                      tooltip: "Copy",
+                      onPressed: () {},
+                    ),
+                  ],
                 ),
               ),
-            )
-          ],
-        ),
+            ),
+          )
+        ],
       ),
     );
   }
