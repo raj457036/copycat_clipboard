b2570f51117db9127f922c6be78f1416b08cec3c
Raj <rs457036@gmail.com>
2024-05-12 14:15:55 +0530

    freemium limitation implemetation ( WIP )

---
+++ lib/constants/plan_features.dart
@@ -0,0 +1,26 @@
+import 'package:clipboard/constants/numbers/duration.dart';
+import 'package:clipboard/constants/numbers/file_sizes.dart';
+
+class PlanFeatures {
+  final int maxSyncCount;
+  final int maxCollection;
+  final int maxClipsPerCollection;
+  final int maxAutoSyncFrequency;
+  final int maxAutoSyncFileSize;
+
+  const PlanFeatures({
+    required this.maxSyncCount,
+    required this.maxCollection,
+    required this.maxClipsPerCollection,
+    required this.maxAutoSyncFrequency,
+    required this.maxAutoSyncFileSize,
+  });
+}
+
+const freePlan = PlanFeatures(
+  maxSyncCount: 100,
+  maxCollection: 2,
+  maxClipsPerCollection: 10,
+  maxAutoSyncFrequency: $30S,
+  maxAutoSyncFileSize: $20MB,
+);
