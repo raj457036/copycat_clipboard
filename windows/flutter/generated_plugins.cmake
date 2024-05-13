74b063c8b6738cf90c66f8d9fc877525565e0b36
rahulsingh2031 <rahulsingh2031965@GMAIL.COM>
2024-05-13 23:45:47 +0530

    build files

--- windows/flutter/generated_plugins.cmake
+++ windows/flutter/generated_plugins.cmake
@@ -11,7 +11,6 @@ list(APPEND FLUTTER_PLUGIN_LIST
   hotkey_manager_windows
   irondash_engine_context
   isar_flutter_libs
-  platform_device_id_windows
   screen_retriever
   share_plus
   super_native_extensions
