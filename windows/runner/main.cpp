6d2475475e90135a9091cacaaf3097c588531d77
Raj <rs457036@gmail.com>
2024-05-28 11:29:11 +0530

    App name chaned to CopyCat

--- windows/runner/main.cpp
+++ windows/runner/main.cpp
@@ -80,7 +80,7 @@ int APIENTRY wWinMain(_In_ HINSTANCE instance, _In_opt_ HINSTANCE prev,
   FlutterWindow window(project);
   Win32Window::Point origin(10, 10);
   Win32Window::Size size(1280, 720);
-  if (!window.Create(L"Clipboard", origin, size))
+  if (!window.Create(L"CopyCat", origin, size))
   {
     return EXIT_FAILURE;
   }
