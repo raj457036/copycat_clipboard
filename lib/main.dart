e301773a6557af8736488bb705c1dcd8ad592e74
Raj <rs457036@gmail.com>
2024-06-01 18:13:36 +0530

    _

--- lib/main.dart
+++ lib/main.dart
@@ -64,7 +64,6 @@ Future<void> main() async {
       windowButtonVisibility: false,
     );
     windowManager.waitUntilReadyToShow(windowOptions).then((_) async {
-      await windowManager.hide();
       await windowManager.setClosable(false);
       await windowManager.setSkipTaskbar(true);
       if (Platform.isMacOS) {
