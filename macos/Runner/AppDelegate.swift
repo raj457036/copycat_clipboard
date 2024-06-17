109a4c981ea51b6d2473a5f18741bc0cecb7cb98
Raj <rs457036@gmail.com>
2024-06-17 19:27:49 +0530

    single instance app!

--- macos/Runner/AppDelegate.swift
+++ macos/Runner/AppDelegate.swift
@@ -4,15 +4,15 @@ import app_links
 
 @NSApplicationMain
 class AppDelegate: FlutterAppDelegate {
+
   override func applicationShouldTerminateAfterLastWindowClosed(_ sender: NSApplication) -> Bool {
-    // return true
     return false
   }
 
-  public override func application(
+  override func application(
     _ application: NSApplication,
     continue userActivity: NSUserActivity,
-    restorationHandler: @escaping ([any NSUserActivityRestoring]) -> Void) -> Bool {
+    restorationHandler: @escaping ([NSUserActivityRestoring]) -> Void) -> Bool {
 
     guard let url = AppLinks.shared.getUniversalLink(userActivity) else {
       return false
@@ -21,5 +21,33 @@ class AppDelegate: FlutterAppDelegate {
     AppLinks.shared.handleLink(link: url.absoluteString)
     
     return false // Returning true will stop the propagation to other packages
+  }
+
+  override func applicationDidFinishLaunching(_ notification: Notification) {
+    if !isSingleInstance() {
+      NSApplication.shared.terminate(self)
+    }
+  }
+
+  override func applicationShouldHandleReopen(_ sender: NSApplication, hasVisibleWindows flag: Bool) -> Bool {
+    if !flag {
+      for window in sender.windows {
+        window.makeKeyAndOrderFront(self)
+      }
+    }
+    return true
+  }
+
+  private func isSingleInstance() -> Bool {
+    let runningApps = NSWorkspace.shared.runningApplications
+    let currentApp = NSRunningApplication.current
+
+    for app in runningApps {
+      if app.bundleIdentifier == currentApp.bundleIdentifier && app.processIdentifier != currentApp.processIdentifier {
+        app.activate(options: [.activateAllWindows, .activateIgnoringOtherApps])
+        return false
+      }
+    }
+    return true
   }
 }
--- scripts/build_mac.sh
+++ scripts/build_mac.sh
@@ -1,1 +1,2 @@
-flutter build macos --dart-define-from-file="local/prod.json"
+flutter build macos --dart-define-from-file="local/prod.json" && \
+create-dmg "build/macos/Build/Products/Release/CopyCat.app" build/macos/Build/Products/Release
