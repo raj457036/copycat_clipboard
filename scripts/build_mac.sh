b087d4db1e11f9b3b1bb3485ce1415afa2c2d580
Raj <rs457036@gmail.com>
2024-06-23 23:17:55 +0530

    updated build script

--- scripts/build_mac.sh
+++ scripts/build_mac.sh
@@ -1,2 +1,7 @@
-flutter build macos --dart-define-from-file="local/prod.json" && \
-create-dmg "build/macos/Build/Products/Release/CopyCat.app" build/macos/Build/Products/Release
+flutter build macos --dart-define-from-file="local/prod.json" \
+&& create-dmg --window-pos 200 120 \                                  
+  --window-size 800 400 \
+  --icon-size 100 \
+  --icon "CopyCat.app" 200 190 \
+  --hide-extension "CopyCat.app" \
+  --app-drop-link 600 185 CopyCat.dmg build/macos/Build/Products/Release/CopyCat.app
