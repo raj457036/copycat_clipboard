baa2946a7e67923d1ae1a058c418ea2143f4a610
Raj <rs457036@gmail.com>
2024-06-30 13:13:10 +0530

    wip

--- android/app/src/main/kotlin/com/entilitystudio/CopyCat/MainActivity.kt
+++ android/app/src/main/kotlin/com/entilitystudio/CopyCat/MainActivity.kt
@@ -1,5 +1,5 @@
 package com.entilitystudio.CopyCat
 
-import io.flutter.embedding.android.FlutterActivity
+import io.flutter.embedding.android.FlutterFragmentActivity
 
-class MainActivity: FlutterActivity()
+class MainActivity: FlutterFragmentActivity()
