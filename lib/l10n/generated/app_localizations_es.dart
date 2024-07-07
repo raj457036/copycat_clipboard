c6d26481f7d6b60ed94722c2449faa41c582cf4b
Raj <rs457036@gmail.com>
2024-07-07 17:28:31 +0530

    added localization

--- lib/l10n/generated/app_localizations_es.dart
+++ lib/l10n/generated/app_localizations_es.dart
@@ -858,4 +858,8 @@ class AppLocalizationsEs extends AppLocalizations {
 
   @override
   String get submit => 'Enviar';
+
+  @override
+  String get subscribeInSupportedPlatform =>
+      'Para acceder a las funciones premium en Copycat Clipboard, suscríbete a través de Play Store o Apple App Store. Tu suscripción se sincronizará en todos tus dispositivos, incluidos Linux y Windows.';
 }
