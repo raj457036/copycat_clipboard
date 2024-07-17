7c37237702f7349f4c4d80dcd16b91bafebdebac
Raj <rs457036@gmail.com>
2024-07-17 23:07:13 +0530

    _

--- scripts/build_mac.sh
+++ scripts/build_mac.sh
@@ -1,7 +1,2 @@
 flutter build macos --dart-define-from-file="local/prod.json" && \
-create-dmg --window-pos 200 120 \
-  --window-size 800 400 \
-  --icon-size 100 \
-  --icon "CopyCat.app" 200 190 \
-  --hide-extension "CopyCat.app" \
-  --app-drop-link 600 185 CopyCat.dmg build/macos/Build/Products/Release/CopyCat.app
+create-dmg --overwrite --identity="Apple Development: Raj Singh (X57UNFWL7W)" build/macos/Build/Products/Release/CopyCat.app
