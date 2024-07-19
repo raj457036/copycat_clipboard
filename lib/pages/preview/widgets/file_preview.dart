2570fe149f03b70e8e0c91cb6746e030db4bb5a4
Raj <rs457036@gmail.com>
2024-07-19 23:40:50 +0530

    _

--- lib/pages/preview/widgets/file_preview.dart
+++ lib/pages/preview/widgets/file_preview.dart
@@ -1,6 +1,9 @@
 import 'package:clipboard/constants/widget_styles.dart';
 import 'package:clipboard/db/clipboard_item/clipboard_item.dart';
 import 'package:clipboard/l10n/l10n.dart';
+import 'package:clipboard/utils/clipboard_actions.dart';
+import 'package:clipboard/utils/common_extension.dart';
+import 'package:clipboard/utils/utility.dart';
 import 'package:flutter/material.dart';
 
 class FileClipPreviewCard extends StatelessWidget {
@@ -12,8 +15,14 @@ class FileClipPreviewCard extends StatelessWidget {
     required this.isMobile,
   });
 
+  void open() async {
+    openFile(item);
+  }
+
   @override
   Widget build(BuildContext context) {
+    final textTheme = context.textTheme;
+    final colors = context.colors;
     return Card.filled(
       margin: isMobile
           ? const EdgeInsets.only(
@@ -32,8 +41,42 @@ class FileClipPreviewCard extends StatelessWidget {
       child: Center(
         child: SingleChildScrollView(
           padding: const EdgeInsets.all(padding16),
-          child: SelectableText(
-            item.fileName ?? context.locale.nothingHere,
+          child: Column(
+            crossAxisAlignment: CrossAxisAlignment.center,
+            mainAxisAlignment: MainAxisAlignment.center,
+            mainAxisSize: MainAxisSize.min,
+            children: [
+              Text.rich(
+                TextSpan(text: item.fileName?.sub(end: 10), children: [
+                  if (item.fileMimeType != null)
+                    TextSpan(
+                      text: "\n(${item.fileMimeType})",
+                      style: textTheme.labelMedium?.copyWith(
+                        color: colors.onTertiaryContainer,
+                      ),
+                    ),
+                  if (item.fileSize != null)
+                    TextSpan(
+                      text: "\n${formatBytes(item.fileSize!)}",
+                      style: textTheme.labelSmall?.copyWith(
+                        color: colors.onTertiaryContainer,
+                      ),
+                    )
+                ]),
+                overflow: TextOverflow.fade,
+                maxLines: 5,
+                textAlign: TextAlign.center,
+                style: textTheme.titleMedium
+                    ?.copyWith(color: colors.onTertiaryContainer, height: 1.8),
+              ),
+              if (item.inCache) height12,
+              if (item.inCache)
+                ElevatedButton.icon(
+                  icon: const Icon(Icons.open_in_new),
+                  onPressed: open,
+                  label: Text(context.locale.open),
+                )
+            ],
           ),
         ),
       ),
