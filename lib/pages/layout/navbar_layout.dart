c6a99dafe4f9acc2ad959b42739a494e2112f6e4
Raj <rs457036@gmail.com>
2024-07-20 18:46:38 +0530

    _

--- lib/pages/layout/navbar_layout.dart
+++ lib/pages/layout/navbar_layout.dart
@@ -1,6 +1,7 @@
 import 'package:clipboard/common/logging.dart';
 import 'package:clipboard/l10n/l10n.dart';
 import 'package:clipboard/routes/utils.dart';
+import 'package:clipboard/widgets/network_observer.dart';
 import 'package:flutter/material.dart';
 
 class NavBarPage extends StatefulWidget {
@@ -26,53 +27,56 @@ class _NavBarPageState extends State<NavBarPage> {
   Widget build(BuildContext context) {
     final width = MediaQuery.of(context).size.width;
     final smallScreen = width <= 576;
-    final scaffold = Scaffold(
-      body: widget.child,
-      floatingActionButton: smallScreen && widget.depth == 1
-          ? getFloatingActionButton(
-              context,
-              widget.navbarActiveIndex,
-              isMobile: smallScreen,
-            )
-          : null,
-      floatingActionButtonLocation: FloatingActionButtonLocation.endFloat,
-      bottomNavigationBar: smallScreen && widget.depth == 1
-          ? Column(
-              mainAxisSize: MainAxisSize.min,
-              children: [
-                // const HomeBannerAd(
-                //   key: ValueKey("home-banner-ad"),
-                // ),
-                NavigationBar(
-                  selectedIndex: widget.navbarActiveIndex,
-                  onDestinationSelected: (idx) => onNavItemTapped(context, idx),
-                  // labelBehavior: NavigationDestinationLabelBehavior.alwaysHide,
-                  destinations: [
-                    NavigationDestination(
-                      icon: const Icon(Icons.paste_rounded),
-                      label: context.locale.clipboard.substring(0, 8),
-                      tooltip: context.locale.clipboard,
-                    ),
-                    NavigationDestination(
-                      icon: const Icon(Icons.content_paste_search_rounded),
-                      label: context.locale.search,
-                      tooltip: context.locale.searchClipboard,
-                    ),
-                    NavigationDestination(
-                      icon: const Icon(Icons.collections_bookmark),
-                      label: context.locale.collections,
-                      tooltip: context.locale.collections,
-                    ),
-                    NavigationDestination(
-                      icon: const Icon(Icons.settings),
-                      label: context.locale.settings.substring(0, 8),
-                      tooltip: context.locale.settings,
-                    ),
-                  ],
-                ),
-              ],
-            )
-          : null,
+    final scaffold = NetworkObserver(
+      child: Scaffold(
+        body: widget.child,
+        floatingActionButton: smallScreen && widget.depth == 1
+            ? getFloatingActionButton(
+                context,
+                widget.navbarActiveIndex,
+                isMobile: smallScreen,
+              )
+            : null,
+        floatingActionButtonLocation: FloatingActionButtonLocation.endFloat,
+        bottomNavigationBar: smallScreen && widget.depth == 1
+            ? Column(
+                mainAxisSize: MainAxisSize.min,
+                children: [
+                  // const HomeBannerAd(
+                  //   key: ValueKey("home-banner-ad"),
+                  // ),
+                  NavigationBar(
+                    selectedIndex: widget.navbarActiveIndex,
+                    onDestinationSelected: (idx) =>
+                        onNavItemTapped(context, idx),
+                    // labelBehavior: NavigationDestinationLabelBehavior.alwaysHide,
+                    destinations: [
+                      NavigationDestination(
+                        icon: const Icon(Icons.paste_rounded),
+                        label: context.locale.clipboard.substring(0, 8),
+                        tooltip: context.locale.clipboard,
+                      ),
+                      NavigationDestination(
+                        icon: const Icon(Icons.content_paste_search_rounded),
+                        label: context.locale.search,
+                        tooltip: context.locale.searchClipboard,
+                      ),
+                      NavigationDestination(
+                        icon: const Icon(Icons.collections_bookmark),
+                        label: context.locale.collections,
+                        tooltip: context.locale.collections,
+                      ),
+                      NavigationDestination(
+                        icon: const Icon(Icons.settings),
+                        label: context.locale.settings.substring(0, 8),
+                        tooltip: context.locale.settings,
+                      ),
+                    ],
+                  ),
+                ],
+              )
+            : null,
+      ),
     );
     return PopScope(
       canPop: false,
