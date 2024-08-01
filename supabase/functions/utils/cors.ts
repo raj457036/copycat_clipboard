528f9d9de4adee2e0a5c75365ae7724593c16bdd
Raj <rs457036@gmail.com>
2024-08-01 21:54:42 +0530

    refactors

--- supabase/functions/utils/cors.ts
+++
@@ -1,5 +0,0 @@
-export const corsHeaders = {
-  "Access-Control-Allow-Origin": "*",
-  "Access-Control-Allow-Headers": "authorization, x-client-info, apikey",
-  "Access-Control-Allow-Methods": "POST, GET, OPTIONS",
-};
