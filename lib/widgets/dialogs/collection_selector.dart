0e0affd35b42744a93971f8eac247a2c8df428e3
Raj <rs457036@gmail.com>
2024-08-03 01:40:47 +0530

    _

--- lib/widgets/dialogs/collection_selector.dart
+++ lib/widgets/dialogs/collection_selector.dart
@@ -7,6 +7,7 @@ import 'package:copycat_base/l10n/l10n.dart';
 import 'package:copycat_base/utils/common_extension.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
+import 'package:go_router/go_router.dart';
 
 class ClipCollectionSelectionDialog extends StatelessWidget {
   const ClipCollectionSelectionDialog({super.key});
@@ -70,7 +71,7 @@ class ClipCollectionSelectionDialog extends StatelessWidget {
                     subtitle: collection.description != null
                         ? Text(collection.description!)
                         : null,
-                    onTap: () => Navigator.pop(context, collection),
+                    onTap: () => context.pop(collection),
                   );
                 },
               );
