ece3fef20543a704c433d445e881b9d19df61a22
Raj <rs457036@gmail.com>
2024-08-11 02:45:48 +0530

    _

--- lib/widgets/clip_card.dart
+++ lib/widgets/clip_card.dart
@@ -1,6 +1,7 @@
 import 'package:clipboard/widgets/clip_cards/clip_card_body.dart';
 import 'package:clipboard/widgets/clip_cards/clip_menu.dart';
 import 'package:copycat_base/db/clipboard_item/clipboard_item.dart';
+import 'package:copycat_pro/widgets/drag_drop/drag_item.dart';
 import 'package:flutter/material.dart';
 
 class ClipCard extends StatelessWidget {
@@ -17,12 +18,15 @@ class ClipCard extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return ClipCardMenu(
+    return DraggableItem(
       item: item,
-      child: ClipCardBody(
+      child: ClipCardMenu(
         item: item,
-        focused: autoFocus,
-        canPaste: canPaste,
+        child: ClipCardBody(
+          item: item,
+          focused: autoFocus,
+          canPaste: canPaste,
+        ),
       ),
     );
   }
